4a32f2d035477599e58cdbdce76884a85cba6732
Raj Singh <118312686+yorkzap@users.noreply.github.com>
2025-01-21 19:06:20 -0800

    Add nav links

--- src/components/Navbar/navData.js
+++ src/components/Navbar/navData.js
@@ -20,17 +20,12 @@ export const navLinks = [
 export const socialLinks = [
   {
     label: 'Github',
-    url: 'https://github.com/mayankjain25',
+    url: 'https://github.com/yorkzap',
     icon: 'github',
   },
   {
     label: 'Linkedin',
-    url: 'https://www.linkedin.com/in/mayank-jain-688409208/',
+    url: 'https://www.linkedin.com/in/rajgursher-singh/',
     icon: 'linkedin',
-  },
-  {
-    label: 'Leetcode',
-    url: 'https://leetcode.com/jainmayank2509/',
-    icon: 'leetcode',
   },
 ];
