0487d3e761d0b978ec743acfdd40547272984882
Raj Singh <118312686+yorkzap@users.noreply.github.com>
2025-01-21 16:46:23 -0800

    Remove Resume

--- src/components/Navbar/navData.js
+++ src/components/Navbar/navData.js
@@ -15,11 +15,6 @@ export const navLinks = [
     label: 'Contact',
     pathname: '/contact',
   },
-  {
-    label: 'Resume',
-    pathname:
-      'https://drive.google.com/file/d/1w0fSvU6YW2pG-HmDci4CAR_KE_K9zE0L/view?usp=sharing',
-  },
 ];
 
 export const socialLinks = [
